4808e764943a2bd700e4567782bfe7f880b026d4
quentintruong <quentintruong@gmail.com>
2016-12-28 07:58:54 -0800

    various catches for errors/exceptions

--- ricefarmer-chem.py
+++ ricefarmer-chem.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import random
-
+import sys
 inc = 0
 periodicDict = {'Hydrogen': 'H', 'Helium': 'He', 'Lithium': 'Li', 'Beryllium': 'Be', 'Boron': 'B', 'Carbon': 'C', 'Nitrogen': 'N', 'Oxygen': 'O', 'Fluorine': 'F', 'Neon': 'Ne', 'Sodium': 'Na', 'Magnesium': 'Mg', 'Aluminium': 'Al', 'Silicon': 'Si', 'Phosphorus': 'P', 'Sulfur': 'S', 'Chlorine': 'Cl', 'Argon': 'Ar', 'Potassium': 'K', 'Calcium': 'Ca', 'Scandium': 'Sc', 'Titanium': 'Ti', 'Vanadium': 'V', 'Chromium': 'Cr', 'Manganese': 'Mn', 'Iron': 'Fe', 'Cobalt': 'Co', 'Nickel': 'Ni', 'Copper': 'Cu', 'Zinc': 'Zn', 'Gallium': 'Ga', 'Germanium': 'Ge', 'Arsenic': 'As', 'Selenium': 'Se', 'Bromine': 'Br', 'Krypton': 'Kr', 'Rubidium': 'Rb', 'Strontium': 'Sr', 'Yttrium': 'Y', 'Zirconium': 'Zr', 'Niobium': 'Nb', 'Molybdenum': 'Mo', 'Technetium': 'Tc', 'Ruthenium': 'Ru', 'Rhodium': 'Rh', 'Palladium': 'Pd', 'Silver': 'Ag', 'Cadmium': 'Cd', 'Indium': 'In', 'Tin': 'Sn', 'Antimony': 'Sb', 'Tellurium': 'Te', 'Iodine': 'I', 'Xenon': 'Xe', 'Cesium': 'Cs', 'Barium': 'Ba', 'Lanthanum': 'La', 'Cerium': 'Ce', 'Praseodymium': 'Pr', 'Neodymium': 'Nd', 'Promethium': 'Pm', 'Samarium': 'Sm', 'Europium': 'Eu', 'Gadolinium': 'Gd', 'Terbium': 'Tb', 'Dysprosium': 'Dy', 'Holmium': 'Ho', 'Erbium': 'Er', 'Thulium': 'Tm', 'Ytterbium': 'Yb', 'Lutetium': 'Lu', 'Hafnium': 'Hf', 'Tantalum': 'Ta', 'Tungsten': 'W', 'Rhenium': 'Re', 'Osmium': 'Os', 'Iridium': 'Ir', 'Platinum': 'Pt', 'Gold': 'Au', 'Mercury': 'Hg', 'Thallium': 'Tl', 'Lead': 'Pb', 'Bismuth': 'Bi', 'Polonium': 'Po', 'Astatine': 'At', 'Radon': 'Rn', 'Francium': 'Fr', 'Radium': 'Ra', 'Actinium': 'Ac', 'Thorium': 'Th', 'Protactinium': 'Pa', 'Uranium': 'U', 'Neptunium': 'Np', 'Plutonium': 'Pu', 'Americium': 'Am', 'Curium': 'Cm', 'Berkelium': 'Bk', 'Californium': 'Cf', 'Einsteinium': 'Es', 'Fermium': 'Fm', 'Mendelevium': 'Md', 'Nobelium': 'No', 'Lawrencium': 'Lr', 'Rutherfordium': 'Rf', 'Dubnium': 'Db', 'Seaborgium': 'Sg', 'Bohrium': 'Bh', 'Hassium': 'Hs', 'Meitnerium': 'Mt', 'Darmstadtium': 'Ds', 'Roentgenium': 'Rg', 'Ununbium': 'Uub', 'Ununtrium': 'Uut', 'Ununquadium': 'Uuq', 'Ununpentium': 'Uup', 'Ununhexium': 'Uuh', 'Ununseptium': 'Uus', 'Ununoctium': 'Uuo'}
 
@@ -29,7 +29,7 @@ class RiceSpider(scrapy.Spider):
         username.send_keys('username')
 
         pw = self.driver.find_element_by_xpath('//input[@id="edit-pass"]')
-        pw.send_keys('password')
+        pw.send_keys('quentintruong')
         pw.submit()
 
         self.driver.get('http://freerice.com/#/chemical-symbols-full-list/1')
@@ -42,13 +42,22 @@ class RiceSpider(scrapy.Spider):
         riceSRC = self.driver.page_source
         option = [0,1,2,3,4]
 
-        option[0] = Selector(text=riceSRC).xpath('//div[@id="question-title"]/a/b/text()').extract()[0]
-        option[1] = Selector(text=riceSRC).xpath('//a[@class="answer-item" and @rel="0"]/text()').extract()[0]
-        option[2] = Selector(text=riceSRC).xpath('//a[@class="answer-item" and @rel="1"]/text()').extract()[0]
-        option[3] = Selector(text=riceSRC).xpath('//a[@class="answer-item" and @rel="2"]/text()').extract()[0]
-        option[4] = Selector(text=riceSRC).xpath('//a[@class="answer-item" and @rel="3"]/text()').extract()[0]
+        option[0] = Selector(text=riceSRC).xpath('//div[@id="question-title"]/a/b/text()').extract_first()
+        option[1] = Selector(text=riceSRC).xpath('//a[@class="answer-item" and @rel="0"]/text()').extract_first()
+        option[2] = Selector(text=riceSRC).xpath('//a[@class="answer-item" and @rel="1"]/text()').extract_first()
+        option[3] = Selector(text=riceSRC).xpath('//a[@class="answer-item" and @rel="2"]/text()').extract_first()
+        option[4] = Selector(text=riceSRC).xpath('//a[@class="answer-item" and @rel="3"]/text()').extract_first()
 
-        mySym = periodicDict[option[0]]
+        if None in option:
+            self.driver.quit()
+            sys.exit()
+
+        try:
+            mySym = periodicDict[option[0]]
+        except KeyError:
+            self.driver.quit()
+            sys.exit()
+
         answer = ""
 
         if mySym == option[1]:
@@ -57,7 +66,7 @@ class RiceSpider(scrapy.Spider):
             answer = self.driver.find_element_by_xpath('//a[@class="answer-item" and @rel="1"]')
         elif mySym == option[3]:
             answer = self.driver.find_element_by_xpath('//a[@class="answer-item" and @rel="2"]')
-        elif mySym == option[4]:
+        else:
             answer = self.driver.find_element_by_xpath('//a[@class="answer-item" and @rel="3"]')
 
         answer.click()
@@ -65,8 +74,10 @@ class RiceSpider(scrapy.Spider):
         inc += 1
         if inc == 19: #stop beyond 19 because website ban
             self.driver.quit()
+            sys.exit()
 
         self.driver.get('http://freerice.com/restart')
+
         yield scrapy.Request('http://freerice.com/restart', callback = self.parseRice, dont_filter = True)
 
         
